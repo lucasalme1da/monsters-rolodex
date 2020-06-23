1e1134cfbd1d0b1c08f69af6bf94edc58ea39859
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2020-06-23 15:53:11 -0300

    Updates

---
+++ precache-manifest.14c2ceac32fcc58d89f86077560b057e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be691a4897f011b42ac4d67d1756d53f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d78b1f16b72c61b95f8a",
+    "url": "/monsters-rolodex/static/css/main.5663926b.chunk.css"
+  },
+  {
+    "revision": "874ef6dce6428a3044f2",
+    "url": "/monsters-rolodex/static/js/2.3f91b9d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3f91b9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d78b1f16b72c61b95f8a",
+    "url": "/monsters-rolodex/static/js/main.c8e1ddf5.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
